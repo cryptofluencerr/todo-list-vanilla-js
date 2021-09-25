f5f6da3de58b2cf90ee29947a481fa7f7748b52d
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-09-25 18:23:03 +0530

    update

---
+++ script.js
@@ -0,0 +1,56 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener(
+  "click", //Functions
+  function addTodo(event) {
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-list");
+    newTodo.innerText = todoInput.value;
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.classList.add("complete-btn");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    todoDiv.appendChild(completedButton);
+
+    //TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-btn");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDiv.appendChild(trashButton);
+
+    //APPEND TODO LIST
+    todoList.appendChild(todoDiv);
+
+    //Clear Todo INPUT VALUE
+    todoInput.value = "";
+  }
+);
+
+todoList.addEventListener("click", function deleteCheck(e) {
+  const item = e.target;
+  //DELETE TODO
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+  //CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement; //also understand what event does in JS
+    todo.classList.toggle("completed"); //understand what this code is doing
+  }
+});
